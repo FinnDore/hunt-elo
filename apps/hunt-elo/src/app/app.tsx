748716384c23e80a37aa65f39d36e808df695dff
Finn <34718806+FinnDore@users.noreply.github.com>
2022-02-13 16:38:02 +0000

    chore(hunt-elo): added two workflows for versioning and building

--- apps/hunt-elo/src/app/app.tsx
+++ apps/hunt-elo/src/app/app.tsx
@@ -1,14 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <>
-      <NxWelcome title="hunt-elo" />
-      <div />
-    </>
-  );
+  return <>hello World</>;
 }
 
 export default App;
