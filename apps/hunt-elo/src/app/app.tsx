c68d30cb018fab2334e2383f29394570768656d7
Finn <34718806+FinnDore@users.noreply.github.com>
2022-02-20 15:20:42 +0000

    chore(hunt-elo): removed unneeded imports

--- apps/hunt-elo/src/app/app.tsx
+++ apps/hunt-elo/src/app/app.tsx
@@ -1,20 +1,12 @@
-import SettingsIcon from '@mui/icons-material/Settings';
-import {
-    createTheme,
-    CssBaseline,
-    IconButton,
-    ThemeProvider,
-} from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import classes from './app.module.scss';
 import Elo from './features/elo/elo';
-import EloDisplay from './features/elo/elo-display/elo-display';
 import { SettingsOverlay } from './features/settings/settings-overlay';
 import { ViewContainer } from './features/view-container/view-container';
 import TitleBar from './title-bar/title-bar';
 import { ActiveOverlay } from './_enums/current-overlay';
-import { setActiveOverlay } from './_store/_actions/settings/set-active-overlay.action';
 import { themeModeSelector } from './_store/_selectors/settings/theme-mode.selector';
 
 /**
